d98a7f4b53d0ac37e58047aeef5a75543f69d403
yahav1w <yahavochana111@gmail.com>
2025-01-02 21:39:59 +0200

    King files

--- King.cpp
+++ King.cpp
@@ -1,5 +1,8 @@
 #include "King.h"
 
+//the function return if the player can move the move he wanted to make and if not return why
+//move - the move the player wanted to make in this struct <dest><source> for example: "e4e5"
+//the function return the code that need to sent to the fronted
 int King::Move(std::string move)
 {
     int code = 0;
@@ -10,8 +13,12 @@ int King::Move(std::string move)
     return code;
 }
 
+//the function check if the move the player wants to do is valid
+//move - the move the player wanted to make in this struct <dest><source> for example: "e4e5"
+//the function return the code that need to send to the fronted
 int King::IsValid(std::string move)
 {
+    bool codesix = true;
     int i = 0, j = 0, k = 0;
     if (move[0] > CELLING || move[0] < FLOOR ||//checking if the source height is in the board
         move[2] > CELLING || move[2] < FLOOR ||//checking if the destination height is in the board
@@ -25,7 +32,14 @@ int King::IsValid(std::string move)
         return 7;
     }
     //check code 7 is before code 6 because 7 is a specific code 6 check
-    bool codesix = true;
+    //check by this table
+    // i | j | k | row   | coll
+    // 0 | 0 | 0 | row+0 | coll+1
+    // 0 | 0 | 1 | row+1 | coll+1
+    // 0 | 1 | 0 | row+0 | coll-1
+    // 0 | 1 | 1 | row+1 | coll-1
+    // 1 | 0 | 0 | row-1 | coll+1
+    // 1 | 0 | 1 | row-1 | coll-1
     for (i = 0; i < 2;i++)
     {
         for (j = 0;j < 2 ;j++)
@@ -38,13 +52,13 @@ int King::IsValid(std::string move)
                 }
             }
         }
-        if (i == 1 && j == 1)
+        if (i == 1 && j == 1)//i tried to connect the two last option but i didnt succed so i write them after
         {
             break;
         }
     }
-    if (std::string(1, move[0]) + move[1] == std::string(1, static_cast<char>(move[2] + 1)) + static_cast<char>(move[3]) ||
-        std::string(1, move[0]) + move[1] == std::string(1, static_cast<char>(move[2] - 1)) + static_cast<char>(move[3]))
+    if (std::string(1, move[0]) + move[1] == std::string(1, static_cast<char>(move[2] + 1)) + static_cast<char>(move[3]) ||// row+1 | coll
+        std::string(1, move[0]) + move[1] == std::string(1, static_cast<char>(move[2] - 1)) + static_cast<char>(move[3]))  // row-1 | coll
     {
         codesix = false;
     }
