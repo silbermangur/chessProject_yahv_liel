ed3f4b59395ef66adaae0fd0596adce7f87644aa
yahav1w <yahavochana111@gmail.com>
2025-01-04 01:42:30 +0200

    Abstract piece file

--- IPiece.h
+++ IPiece.h
@@ -9,7 +9,7 @@
 
 class IPiece
 {
-protected:
-	virtual int Move(std::string) = 0;//The function that move the piece if the piece can move
+public:
+	virtual int Move(std::string,IPiece* board[8][8]) = 0;//The function that move the piece if the piece can move
 	virtual int IsValid(std::string) = 0;//the function check if the piece can move
 };
