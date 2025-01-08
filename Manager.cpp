af3215e638c1ad2bb17b454dca1d5b8fcadc8413
yahav1w <yahavochana111@gmail.com>
2025-01-08 21:11:23 +0200

    manager files

--- Manager.cpp
+++ Manager.cpp
@@ -26,8 +26,8 @@ void Manager::play()
 			codeMsg[0] = '2';
 		}
 		//check error code 3
-		else if (whiteTurn && isupper(gameString[(RIGHT_WALL - move[DST_NUM]) * 8 + (move[DST_LETTER] - FLOOR +1)]) ||//white represented by an upercase letter
-			!whiteTurn && islower(gameString[(RIGHT_WALL - move[DST_NUM]) * 8 + (move[DST_LETTER] - FLOOR +1)]))//black represent by lowercase letter
+		else if (whiteTurn && isupper(gameString[(RIGHT_WALL - move[3]) * 8 + (move[2] - FLOOR +1)]) ||//white represented by an upercase letter
+			!whiteTurn && islower(gameString[(RIGHT_WALL - move[3]) * 8 + (move[2] - FLOOR +1)]))//black represent by lowercase letter
 		{
 			codeMsg[0] = '3';
 		}
@@ -40,6 +40,10 @@ void Manager::play()
 		{
 			pieceCode = piece->Move(move,board);
 			codeMsg[0] = static_cast<char>(pieceCode);
+			if (codeMsg[0] == '0' && isBlock(piece,move))
+			{
+				codeMsg[0] = '6';
+			}
 		}
 		if (codeMsg[0] != CODE_PASS && isCheck(move))
 		{
@@ -154,7 +158,189 @@ Pipe Manager::connectFronted()
 //the function return the piece that the player want to move 
 IPiece* Manager::type(std::string move)
 {
-	return board[move[SRC_LETTER] - 'a' + 1][move[SRC_NUM] - '0'];
+	return board[move[0] - 'a' + 1][move[1] - '0'];
+}
+
+bool Manager::isBlock(IPiece* piece, std::string move)
+{
+	if (piece->IsValid("e4e5") && piece->IsValid("e4d3") && !piece->IsValid("e4e6"))//check if it a king
+	{
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr)//check if the place is clear
+		{
+			return false;
+		}
+		return true;
+	}
+	else if (piece->IsValid("e4e5") && piece->IsValid("e4d3") && piece->IsValid("e4e6"))//check if it a queen
+	{
+		int joinToI = 1,joinToJ = 1;
+		int iTo = 8, jTo = 8;
+		bool foundPiece = false;
+		if (move[SRC_LETTER] - move[DST_LETTER] == move[SRC_NUM] - move[DST_NUM])//the queen move like bishop
+		{
+			if (move[SRC_LETTER] - move[DST_LETTER] > 0)
+			{
+				joinToJ = -1;
+				iTo = 0;
+			}
+			if (move[SRC_NUM] - move[DST_NUM] > 0)
+			{
+				joinToI = 1;
+				jTo = 0;
+			}
+			for (int i = (RIGHT_WALL -  move[SRC_NUM] + 1);i < RIGHT_WALL - move[DST_NUM];i+=joinToI)
+			{
+				for (int j = (move[SRC_LETTER] - FLOOR + 1);j < move[DST_LETTER] - FLOOR;j += joinToJ)
+				{
+					if (board[RIGHT_WALL - move[SRC_NUM] + i][move[SRC_LETTER] - FLOOR + j] != nullptr)
+					{
+						foundPiece = true;
+					}
+				}
+			}
+
+		}
+		else//move like a rook
+		{
+			if (move[SRC_LETTER] == move[DST_LETTER])
+			{
+				if (move[SRC_NUM] - move[DST_NUM] < 0)
+				{
+					joinToI = -1;
+				}
+				for (int i = static_cast<int>(RIGHT_WALL - move[SRC_NUM]);i < RIGHT_WALL - move[DST_NUM];i += joinToI)
+				{
+					if (board[RIGHT_WALL - move[SRC_LETTER]][i] != nullptr)
+					{
+						foundPiece = true;
+					}
+				}
+			}
+			if (move[SRC_NUM] == move[DST_NUM])
+			{
+				if (move[SRC_LETTER] - move[DST_LETTER] < 0)
+				{
+					joinToJ = -1;
+				}
+				for (int J = static_cast<int>(CELLING - move[SRC_LETTER]);J < CELLING - move[DST_LETTER];J += joinToI)
+				{
+					if (board[J][move[SRC_NUM] - FLOOR] != nullptr)
+					{
+						foundPiece = true;
+					}
+				}
+			}
+		}
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr && //first need to be check if there are a piece in the place
+			board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR]->isWhite && piece->isWhite)//check if at the dest it the same color
+		{
+			foundPiece = true;
+		}
+		if (foundPiece)
+		{
+			return false;
+		}
+		return true;
+	}
+	else if (piece->IsValid("b2d3"))//check if it a Knight
+	{
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr && //first need to be check if there are a piece in the place
+			board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR]->isWhite == piece->isWhite)//check if at the dest it the same color
+		{
+			return false;
+		}
+		return true;
+	}
+	else if (piece->IsValid("e4e5") && piece->IsValid("e4f3") && !piece->IsValid("e4d3"))//check if it pawn
+	{
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr || // 
+			board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR]->isWhite == piece->isWhite)
+		{
+			return false;
+		}
+		return true;
+	}
+	else if (piece->IsValid("e4e7"))//check if it a rook
+	{
+		int joinToI = 1, joinToJ = 1;
+		int iTo = 8, jTo = 8;
+		bool foundPiece = false;
+		if (move[SRC_LETTER] == move[DST_LETTER])
+		{
+			if (move[SRC_NUM] - move[DST_NUM] < 0)
+			{
+				joinToI = -1;
+			}
+			for (int i = static_cast<int>(RIGHT_WALL - move[SRC_NUM]);i < RIGHT_WALL - move[DST_NUM];i += joinToI)
+			{
+				if (board[RIGHT_WALL - move[SRC_LETTER]][i] != nullptr)
+				{
+					foundPiece = true;
+				}
+			}
+		}
+		if (move[SRC_NUM] == move[DST_NUM])
+		{
+			if (move[SRC_LETTER] - move[DST_LETTER] < 0)
+			{
+				joinToJ = -1;
+			}
+			for (int J = static_cast<int>(CELLING - move[SRC_LETTER]);J < CELLING - move[DST_LETTER];J += joinToI)
+			{
+				if (board[J][move[SRC_NUM] - FLOOR] != nullptr)
+				{
+					foundPiece = true;
+				}
+			}
+		}
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr && //first need to be check if there are a piece in the place
+			board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR]->isWhite && piece->isWhite)//check if at the dest it the same color
+		{
+			foundPiece = true;
+		}
+		if (foundPiece)
+		{
+			return false;
+		}
+		return true;
+	}
+	else//it a bishop
+	{
+		int joinToI = 1, joinToJ = 1;
+		int iTo = 8, jTo = 8;
+		bool foundPiece = false;
+		if (move[SRC_LETTER] - move[DST_LETTER] > 0)
+		{
+			joinToJ = -1;
+			iTo = 0;
+		}
+		if (move[SRC_NUM] - move[DST_NUM] > 0)
+		{
+			joinToI = 1;
+			jTo = 0;
+		}
+		for (int i = (RIGHT_WALL - move[SRC_NUM] + 1);i < RIGHT_WALL - move[DST_NUM];i += joinToI)
+		{
+			for (int j = (move[SRC_LETTER] - FLOOR + 1);j < move[DST_LETTER] - FLOOR;j += joinToJ)
+			{
+				if (board[RIGHT_WALL - move[SRC_NUM] + i][move[SRC_LETTER] - FLOOR + j] != nullptr)
+				{
+					foundPiece = true;
+				}
+			}
+		}
+		if (board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] != nullptr && //first need to be check if there are a piece in the place
+			board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR]->isWhite && piece->isWhite)//check if at the dest it the same color
+		{
+			foundPiece = true;
+		}
+		if (foundPiece)
+		{
+			return false;
+		}
+		return true;
+	}
+	return false;
 }
 
 //the function check if the current move create a self check
@@ -162,10 +348,10 @@ IPiece* Manager::type(std::string move)
 //the function return true if it does create a self check if not return false
 bool Manager::selfCheck(std::string move, IPiece* piece, bool whiteTurn)
 {
-	IPiece* killed = board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR];//saving the killed piece because it still can be an ilegal move
+	IPiece* killed = board[RIGHT_WALL - move[3]][move[2] - FLOOR];//saving the killed piece because it still can be an ilegal move
 	//do the move requsted
-	board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] = board[RIGHT_WALL - move[SRC_NUM]][move[0] - FLOOR];
-	board[RIGHT_WALL - move[SRC_NUM]][move[SRC_LETTER] - FLOOR] = nullptr;
+	board[RIGHT_WALL - move[3]][move[2] - FLOOR] = board[RIGHT_WALL - move[1]][move[0] - FLOOR];
+	board[RIGHT_WALL - move[1]][move[0] - FLOOR] = nullptr;
 	//variables for the king place
 	std::string kingPlace;
 	bool kingfound = false;
@@ -180,11 +366,11 @@ bool Manager::selfCheck(std::string move, IPiece* piece, bool whiteTurn)
 			if (board[i][j]->IsValid("e4e5") && board[i][j]->IsValid("e4d3") && !board[i][j]->IsValid("e4e6") && board[i][j]->isWhite != whiteTurn)//check if it a king
 			{																								  //(by a combination of moves only the king return answars like this)
 				kingPlace = std::string(1, static_cast<char>(i + FLOOR)) + static_cast<char>(8 - j);
-				kingfound = !kingfound;
+				!kingfound;
 			}
 		}
 	}
-
+	
 	//checking if a check is heppening
 	for (int i = 0; i < 8;i++)
 	{
@@ -194,13 +380,13 @@ bool Manager::selfCheck(std::string move, IPiece* piece, bool whiteTurn)
 			{
 				moveToKing = std::string(2, static_cast<char>(i + FLOOR)) + static_cast<char>(8 - j) + kingPlace;
 				//checking if the move is avaliable  for the piece to do
-				if (board[i][j]->IsValid(moveToKing) == 0 && isBlock(piece, moveToKing))
+				if (board[i][j]->IsValid(moveToKing) == 0 && isBlock(piece,moveToKing))
 				{
 					//doing the move backwards to does mot destroyd the game
-					board[RIGHT_WALL - move[SRC_NUM]][move[SRC_LETTER] - FLOOR] = board[RIGHT_WALL - move[3]][move[DST_LETTER] - FLOOR];
+					board[RIGHT_WALL - move[1]][move[0] - FLOOR] = board[RIGHT_WALL - move[3]][move[2] - FLOOR];
 
 					//returning the piece that was killed to her place
-					board[RIGHT_WALL - move[DST_NUM]][move[DST_LETTER] - FLOOR] = killed;
+					board[RIGHT_WALL - move[3]][move[2] - FLOOR] = killed;
 
 					//if ths piece avaliable of the move returning true
 					return true;
@@ -228,5 +414,3 @@ int main()
 
 	return 1;
 }
-
-
